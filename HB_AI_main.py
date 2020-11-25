47fab6d78721faea148aafa2a7e8ae09aeb90ffe
Timothy Anglea <mathman93@gmail.com>
2020-11-25 14:45:57 -0500

    Initial game mode implementation

--- HB_AI_main.py
+++ HB_AI_main.py
@@ -108,51 +108,87 @@ def get_solutions(lvl, clrs):
 		sols_list.append("".join(x for x in list(sol)))
 		#print("".join(x for x in list(sol))) # Include for debugging
 	#print(len(sols_list)) # Include for debugging
-	poss_sols = sols_list[:] # Copy available solutions
-	return [sols_list, poss_sols]
+	return sols_list
+
+def reset_possible(sols_list):
+	return sols_list[:] # Copy list of available solutions
 
 print("Welcome to Hit & Blow (aka Mastermind).")
+while True:
+	try:
+		print("Which mode would you like to use?")
+		#print("Mode 0 -> CPU play against itself")
+		print("Mode: 1 -> Play against CPU")
+		print("Mode: 2 -> Play with CPU assist")
+		print("Mode: 3 -> Manual CPU assist")
+		game_mode = int(input("Mode: "))
+		if game_mode < 0 or game_mode > 3:
+			print("Not a valid game mode. Please select from the available modes.")
+			continue
+		# End if
+		break
+	except ValueError:
+		print("Not a valid game mode. Please enter an number.")
+		continue
 
 # Game Setup - Create all possible solutions
 [game_lvl, colors] = get_level()
-[all_solutions_list, possible_solutions] = get_solutions(game_lvl, colors)
-
-# Pseudo Solution - Have the program pick a solution
-the_solution = all_solutions_list[random.randint(0,len(all_solutions_list)-1)]
-#print("The Solution: {0}".format(the_solution))
-
 # Game Instructions
-print("Available colors are {0}.".format(colors))
+if game_lvl % 2 == 1:
+	print("Available colors are {0}. Duplicates are not allowed.".format(colors))
+else:
+	print("Available colors are {0}. Duplicates are allowed.".format(colors))
 
-# User Attempts
-attmpt_num = 1 # Initialize attempt number for first guess
-while True:
-	if attmpt_num > 8:
-		print("Too many guesses. Sorry.")
-		print("Actual solution: {0}".format("".join(the_solution)))
-		break
-	attempt = get_attempt(attmpt_num, all_solutions_list)
-	[hits,blows] = check_attempt(attempt, the_solution)
-	print("Hits: {0}; Blows: {1}".format(hits,blows))
-	if hits == 4: # Win Condition
-		print("Congratulations! You win!")
-		break
-	# Find remaining valid solutions based on current attempt.
-	sol_remove = [] # list of solutions to remove; they're invalid
-	for possible in possible_solutions: # For each possible solution
-		[hp,bp] = check_attempt(attempt, possible) # find hits,blows for the attempt if possible is the solution
-		if hp == hits and bp == blows: # If the hits,blows match...
-			continue # possible is still a valid solution
-		sol_remove.append(possible) # Otherwise, it will be removed
-	for x in sol_remove: # For each solution to remove
-		possible_solutions.remove(x) # Remove it from the valid solutions
+all_solutions_list = get_solutions(game_lvl, colors)
+
+if game_mode in [1,2]: # If playing against CPU...
+	# Pseudo Solution - Have the CPU pick a solution
+	the_solution = all_solutions_list[random.randint(0,len(all_solutions_list)-1)]
+	#print("The Solution: {0}".format(the_solution))
 	
-	# Include for assistance
-	print("Possible solutions left: {0}".format(len(possible_solutions)))
-	if len(possible_solutions) < 11:
-		print("; ".join(["".join([x for x in s]) for s in possible_solutions]))
-	
-	# Continue to next attempt
-	attmpt_num += 1
+	# User Attempts
+	attmpt_num = 1 # Initialize attempt number for first guess
+	possible_solutions = reset_possible(all_solutions_list)
+	while True:
+		if attmpt_num > 8:
+			print("Too many guesses. Sorry.")
+			print("Actual solution: {0}".format("".join(the_solution)))
+			break
+		# End if
+		attempt = get_attempt(attmpt_num, all_solutions_list)
+		[hits,blows] = check_attempt(attempt, the_solution)
+		print("Hits: {0}; Blows: {1}".format(hits,blows))
+		if hits == 4: # Win Condition
+			print("Congratulations! You win!")
+			break
+		# End if
+		if game_mode == 2:
+			# Find remaining valid solutions based on current attempt.
+			sol_remove = [] # list of solutions to remove; they're invalid
+			for possible in possible_solutions: # For each possible solution
+				[hp,bp] = check_attempt(attempt, possible) # find hits,blows for the attempt if possible is the solution
+				if hp == hits and bp == blows: # If the hits,blows match...
+					continue # possible is still a valid solution
+				# End if
+				sol_remove.append(possible) # Otherwise, it will be removed
+			# End for
+			for x in sol_remove: # For each solution to remove
+				possible_solutions.remove(x) # Remove it from the valid solutions
+			# End for
+			
+			# Include for assistance
+			print("Possible solutions left: {0}".format(len(possible_solutions)))
+			if len(possible_solutions) < 21:
+				print("; ".join(["".join([x for x in s]) for s in possible_solutions]))
+			# End if
+		# End if
+		# Continue to next attempt
+		attmpt_num += 1
+	# End while
+elif game_mode in [3]:
+	print("Sorry. This mode has not yet been implemented.")
+else: #game_mode in [0]
+	print("This is a secret to everyone.")
+# End if
 
 pause = input("Thanks for playing my game! (Press Enter to exit.)")
