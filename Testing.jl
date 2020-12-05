d83c31fa02f68d87fa9f69869dbca879bf006d23
Timothy Anglea <mathman93@gmail.com>
2020-12-04 23:24:14 -0500

    having fun with exponentials

--- Testing.jl
+++ Testing.jl
@@ -1,10 +1,10 @@
 # Testing.jl
 using Printf
 function func(num)
-    result = 0
-    term = 1
+    result = big(0.0)
+    term = big(1.0)
     result += term
-    for i1 in 1:100
+    for i1 in 1:400
         term = (term * num) / i1
         result += term
     end
@@ -12,6 +12,10 @@ function func(num)
 end
 
 for x in -2:0.5:2
-    @printf("f(%.1f) = %.9f \n", x, func(x))
+    @printf("f(%.1f) = %.9f \n", x, func(big(x)))
 end
+println(func(70.0))
+println(exp(70.0))
 println(func(complex(0,3.1415926)))
+println(func(complex(0,pi)))
+println(exp(complex(0,pi)))
